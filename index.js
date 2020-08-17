a9c87d758b36a277c96489332c0c54666a4cd39f
Rinat Enikeev <rinat.enikeev@gmail.com>
2020-08-17 10:01:08 +0500

    index js

--- index.js
+++ index.js
@@ -1,81 +1,81 @@
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var ParseDashboard = require('parse-dashboard');
+var path = require('path');
+const fs = require('fs');
+const https = require('https');
+
+var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
+
+// Certificate
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/alliance.vernality.net/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/alliance.vernality.net/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/alliance.vernality.net/chain.pem', 'utf8');
+
+const credentials = {
+    key: privateKey,
+    cert: certificate,
+    ca: ca
+};
+
+if (!databaseUri) {
+    console.log('DATABASE_URI not specified, falling back to localhost.');
+}
 
 var api = new ParseServer({
-  // Parse Server settings
+    databaseURI: databaseUri || 'mongodb://localhost:27017/alliance',
+    cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
+    appId: process.env.APP_ID || 'org.vernality.alliance',
+    masterKey: process.env.MASTER_KEY || 'n2vw8wfMsrm4jDSuLMuspiiseBwOIq18rsq6uQ5p', //Add your master key here. Keep it secret!
+    serverURL: process.env.SERVER_URL || 'https://localhost/parse',  // Don't forget to change to https if needed
+    liveQuery: {
+        classNames: [] // List of classes to support for query subscriptions
+    }
 });
 
 var options = { allowInsecureHTTP: false };
 
 var dashboard = new ParseDashboard({
-  // Parse Dashboard settings
+    "apps": [
+        {
+            "serverURL": "https://localhost/parse",
+            "appId": "org.vernality.alliance",
+            "masterKey": "n2vw8wfMsrm4jDSuLMuspiiseBwOIq18rsq6uQ5p",
+            "appName": "Alliance"
+        }
+    ],
+    "users":
+        [
+            {
+                "user":"rinat",
+                "pass":"AllianceBear1",
+                "apps": [{"appId": "org.vernality.alliance"}]
+            }
+        ]
 }, options);
 
 var app = express();
 
+// Serve static assets from the /public folder
+app.use('/public', express.static(path.join(__dirname, '/public')));
+
 // make the Parse Server available at /parse
 app.use('/parse', api);
+
+// Parse Server plays nicely with the rest of your web routes
+app.get('/', function(req, res) {
+    res.redirect('/dashboard');
+});
 
 // make the Parse Dashboard available at /dashboard
 app.use('/dashboard', dashboard);
 
-var httpServer = require('http').createServer(app);
-httpServer.listen(4040);
+var port = process.env.PORT || 1337;
+var httpsServer = https.createServer(credentials, app);
 
+httpServer.listen(port, function() {
+    console.log('parse-server-example running on port ' + port + '.');
+});
 
-
-//
-//
-// var express = require('express');
-// var ParseServer = require('parse-server').ParseServer;
-// var path = require('path');
-//
-// var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
-//
-// if (!databaseUri) {
-//   console.log('DATABASE_URI not specified, falling back to localhost.');
-// }
-//
-// var api = new ParseServer({
-//   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
-//   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-//   appId: process.env.APP_ID || 'myAppId',
-//   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-//   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
-//   liveQuery: {
-//     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-//   }
-// });
-// // Client-keys like the javascript key or the .NET key are not necessary with parse-server
-// // If you wish you require them, you can set them as options in the initialization above:
-// // javascriptKey, restAPIKey, dotNetKey, clientKey
-//
-// var app = express();
-//
-// // Serve static assets from the /public folder
-// app.use('/public', express.static(path.join(__dirname, '/public')));
-//
-// // Serve the Parse API on the /parse URL prefix
-// var mountPath = process.env.PARSE_MOUNT || '/parse';
-// app.use(mountPath, api);
-//
-// // Parse Server plays nicely with the rest of your web routes
-// app.get('/', function(req, res) {
-//   res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
-// });
-//
-// // There will be a test page available on the /test path of your server url
-// // Remove this before launching your app
-// app.get('/test', function(req, res) {
-//   res.sendFile(path.join(__dirname, '/public/test.html'));
-// });
-//
-// var port = process.env.PORT || 1337;
-// var httpServer = require('http').createServer(app);
-// httpServer.listen(port, function() {
-//     console.log('parse-server-example running on port ' + port + '.');
-// });
-//
-// // This will enable the Live Query real-time server
-// ParseServer.createLiveQueryServer(httpServer);
+// This will enable the Live Query real-time server
+ParseServer.createLiveQueryServer(httpServer);
